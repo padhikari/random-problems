a27c5a03d59b2ef0a86b18ed5ec60bc32bfaf9f6
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-06-20 16:55:15 -0700

    fixed bug

--- src/c/bitwise/insertAintoBbetjtoi.c
+++ src/c/bitwise/insertAintoBbetjtoi.c
@@ -33,7 +33,7 @@ int insertBits(int a, int b, int i, int j){
     int left = all1s << (j+1);
     
     // right shift to make 1s after position i, then 0s
-    int right = ((all1s >> i) - 1);
+    int right = ((1 >> i) - 1);
     
     // all 1s except between i and j
     int mask = left | right;
